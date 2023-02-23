7eadceffb9ffc4247c13615fc9f2a76f3cfb1952
Savva <xaeabaras@yandex.ru>
2023-02-23 23:52:40 +0300

    An empty release to make IntelliJ shut up

---
+++ src/main/java/net/GoBanMelol/ygs/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.GoBanMelol.ygs.mixin;
+
+import net.GoBanMelol.ygs.ygs;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		ygs.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
---
+++ src/main/java/net/GoBanMelol/ygs/ygsClient.java
@@ -0,0 +1,10 @@
+package net.GoBanMelol.ygs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ygsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
