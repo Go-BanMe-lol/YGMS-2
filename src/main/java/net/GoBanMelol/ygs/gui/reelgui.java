486c9c3d1799c89f0c45f1137194544397c66589
Savva <xaeabaras@yandex.ru>
2023-02-24 22:36:30 +0300

    Added text

--- src/main/java/net/GoBanMelol/ygs/gui/reelgui.java
+++ src/main/java/net/GoBanMelol/ygs/gui/reelgui.java
@@ -1,11 +1,22 @@
 package net.GoBanMelol.ygs.gui;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
+import io.github.cottonmc.cotton.gui.widget.WButton;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
+import io.github.cottonmc.cotton.gui.widget.WLabel;
+import net.minecraft.text.Text;
+
 
 public class reelgui extends LightweightGuiDescription {
     public reelgui() {
         WGridPanel root = new WGridPanel();
         setRootPanel(root);
         root.setSize(150, 150);
+
+        WLabel label = new WLabel(Text.literal("Affirm?"));
+        //криво сделано, если не будет работать, то поставлю new WLabel("Affirm?")
+        root.add(label, 1, 1);
+
+
+
     }
 }
