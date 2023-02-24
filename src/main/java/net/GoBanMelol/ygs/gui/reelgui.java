55e58db47b4d51540d36e8da83ec1167c93705b0
Savva <xaeabaras@yandex.ru>
2023-02-24 22:54:56 +0300

    Added text + button with placeholder function

--- src/main/java/net/GoBanMelol/ygs/gui/reelgui.java
+++ src/main/java/net/GoBanMelol/ygs/gui/reelgui.java
@@ -16,6 +16,17 @@ public class reelgui extends LightweightGuiDescription {
         //криво сделано, если не будет работать, то поставлю new WLabel("Affirm?")
         root.add(label, 1, 1);
 
+        WLabel label2 = new WLabel(Text.literal("(Yes) or (No) to confirm."));
+        //аналогично.
+        root.add(label2, 1, 1);
+
+        WButton button = new WButton(Text.literal("Confirm"));
+        button.setOnClick(() -> {
+            // This code runs on the client when you click the button.
+            System.out.println("Comfirmed");
+            //Пока пусть будет заглушкой, потом надо будет заменить на проверку написанного + выдачу/снятие креатива.
+        });
+
 
 
     }
