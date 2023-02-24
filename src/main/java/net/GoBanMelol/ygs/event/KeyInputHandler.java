5ffe35f269b4cab232362c87464ccee261347569
Savva <xaeabaras@yandex.ru>
2023-02-24 13:21:24 +0300

    An draft of open gui keybind

--- src/main/java/net/GoBanMelol/ygs/event/KeyInputHandler.java
+++ src/main/java/net/GoBanMelol/ygs/event/KeyInputHandler.java
@@ -1,4 +1,20 @@
 package net.GoBanMelol.ygs.event;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.option.KeyBinding;
+
 public class KeyInputHandler {
+    public static final String KEY_CATEGORY_1 = "key.category.ygs1";
+    public static final String KEY_CATEGORY_OPENGUI = "key.ygs.opengui";
+
+    public static KeyBinding activationKey;
+
+    public static void registerKeyInputs() {
+        ClientTickEvents.END_CLIENT_TICK.register(Client -> {
+           if(activationKey.wasPressed()) {
+               //Key pressed
+
+           }
+        });
+    }
 }
