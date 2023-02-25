31fe9db5aaaa0d3864684bfa69c705c11f6de025
Savva <xaeabaras@yandex.ru>
2023-02-25 12:48:03 +0300

    Aligned text, but not the buttons.

--- src/main/java/net/GoBanMelol/ygs/gui/reelgui.java
+++ src/main/java/net/GoBanMelol/ygs/gui/reelgui.java
@@ -3,6 +3,7 @@ import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.WButton;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
 import io.github.cottonmc.cotton.gui.widget.WLabel;
+import io.github.cottonmc.cotton.gui.widget.data.HorizontalAlignment;
 import io.github.cottonmc.cotton.gui.widget.data.Texture;
 import io.github.cottonmc.cotton.gui.widget.icon.TextureIcon;
 import net.minecraft.text.Text;
@@ -13,18 +14,20 @@ public class reelgui extends LightweightGuiDescription {
     public reelgui() {
         WGridPanel root = new WGridPanel();
         setRootPanel(root);
-        root.setSize(150, 150);
+        root.setSize(8, 5);
 
         WLabel label = new WLabel(Text.literal("Affirm?"));
         //криво сделано, если не будет работать, то поставлю new WLabel("Affirm?")
-        root.add(label, 1, 1);
+        label.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        root.add(label, 4, 1);
 
         WLabel label2 = new WLabel(Text.literal("(Yes) or (No) to confirm."));
         //аналогично.
-        root.add(label2, 1, 1);
+        label2.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        root.add(label2,4, 2);
 
         WButton button = new WButton(Text.literal("Yes"));
-        root.add(button, 20, 20, 30, 20);
+        root.add(button, 1, 4, 2, 20);
         button.setOnClick(() -> {
             // This code runs on the client when you click the button.
             System.out.println("Yes");
@@ -32,7 +35,7 @@ public class reelgui extends LightweightGuiDescription {
         });
 
         WButton button2 = new WButton(Text.literal("No"));
-        root.add(button2, 40, 20, 30, 20);
+        root.add(button2, 7, 4, 2, 20);
         button2.setOnClick(() -> {
 
             System.out.println("No");
